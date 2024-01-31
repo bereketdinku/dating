5fc8e5b26d0496c8ed5cc56b0632c56682ea09fc
bereketdinku <bereketdinku80@gmail.com>
2024-01-31 02:56:31 -0800

    chat

--- lib/api/notification.dart
+++ lib/api/notification.dart
@@ -53,30 +53,6 @@ class FirebaseApi {
     }
   }
 
-  // void showForegroundNotification(RemoteMessage message) async {
-  //   try {
-  //     const AndroidNotificationDetails androidPlatformChannelSpecifics =
-  //         AndroidNotificationDetails(
-  //       'dbfood',
-  //       'date',
-  //       importance: Importance.max,
-  //       priority: Priority.high,
-  //     );
-  //     const NotificationDetails platformChannelSpecifics =
-  //         NotificationDetails(android: androidPlatformChannelSpecifics);
-
-  //     await _flutterLocalNotificationsPlugin.show(
-  //       0,
-  //       message.notification?.title,
-  //       message.notification?.body,
-  //       platformChannelSpecifics,
-  //       payload: 'item x',
-  //     );
-  //   } catch (e) {
-  //     print("Error showing foreground notification: $e");
-  //   }
-  // }
-
   void handleMessage(RemoteMessage message) {
     try {
       // Your logic to handle the message goes here
@@ -230,60 +206,3 @@ class FirebaseApi {
     )));
   }
 }
-
-// Future<void> initPushNotification() async {
-//   try {
-//     // Get initial message when the app is launched from a terminated state
-//     RemoteMessage? initialMessage =
-//         await FirebaseMessaging.instance.getInitialMessage();
-
-//     // Listen for messages when the app is in the foreground
-//     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-//       print("Foreground Message: $message");
-//       handleMessage(message);
-//     });
-
-//     // Listen for messages when the app is in the background and opened by tapping the notification
-//     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-//       print("Background Message: $message");
-//       handleMessage(message);
-//     });
-
-//     if (initialMessage != null) {
-//       // If there is an initial message, handle it
-//       print("Initial Message: $initialMessage");
-//       handleMessage(initialMessage);
-//     }
-//   } catch (e) {
-//     print("Error initializing push notifications: $e");
-//   }
-// }
-
-// void handleMessage(RemoteMessage message) {
-//   try {
-//     // Your logic to handle the message goes here
-//     // Example: Navigate to the HomeScreen
-//     Get.to(HomeScreen());
-//   } catch (e) {
-//     print("Error handling message: $e");
-//   }
-// }
-
-// Future<void> sendPushNotification() async {
-//   try {
-//     final body = {
-//       // "to":"AAAAwe9tmkg:APA91bGI9ACjGc3NuMC4FHIDyP2SbNzdoBZsLCL3cy_Savr_8TJ51cxfJI3ROrwyqYgizOJT2KNlDmEbZJLUoWXS1LCFfTT4TYumembJkxNG6f8vpk1K9W-fd8PHJ7GgHVF8s6IcOewN"
-
-//       "to":
-//           "fZV2AsGDSu-QmS1M6X1QPU:APA91bEbxq2g9yio67I2UKvsGLtklzlWufVRg0eDQ49Hftmuc4qQtzASb3deLA4_Wsb4owm5VOzHbKQzIiDQXWoHPbVojyYD-r7e_DDL_jpnB5pvUIEA_I76uQMf5byzv1N7JA1rAhYQ",
-//       "notification": {"title": "new message", "body": "hi"}
-//     };
-//     var response = await post(Uri.parse('https://fcm.googleapis.com/fcm/send'),
-//         headers: {
-//           HttpHeaders.contentTypeHeader: 'application/json',
-//           HttpHeaders.authorizationHeader:
-//               "key=AAAAwe9tmkg:APA91bGI9ACjGc3NuMC4FHIDyP2SbNzdoBZsLCL3cy_Savr_8TJ51cxfJI3ROrwyqYgizOJT2KNlDmEbZJLUoWXS1LCFfTT4TYumembJkxNG6f8vpk1K9W-fd8PHJ7GgHVF8s6IcOewN"
-//         },
-//         body: jsonEncode(body));
-//   } catch (e) {}
-// }
